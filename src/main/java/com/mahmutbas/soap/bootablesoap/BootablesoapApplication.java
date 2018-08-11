fd1c3095c7135c1520700e44afa253de6b73ce34
mahmutbas <javaon@yahoo.com>
2018-08-11 23:50:04 +0300

    project initial commit

---
+++ src/main/java/com/mahmutbas/soap/bootablesoap/BootablesoapApplication.java
@@ -0,0 +1,14 @@
+package com.mahmutbas.soap.bootablesoap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootablesoapApplication
+{
+
+    public static void main(String[] args)
+    {
+        SpringApplication.run(BootablesoapApplication.class, args);
+    }
+}
